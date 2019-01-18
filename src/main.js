a3c8ee41595426910d2f4496e33a191a8913805b
Rohan Moore <rohanpmoore@gmail.com>
2019-01-18 14:26:15 -0800

    refactor code

--- src/main.js
+++ src/main.js
@@ -17,14 +17,7 @@ $(document).ready(function() {
     reset();
     let promise = doctor.findForIssue($("#malady").val());
     promise.then(function(response) {
-      let body = JSON.parse(response);
-      if(body.data.length < 1) {
-        $("#error").text(`I'm sorry, there are no doctors in your area matching your search query.`)
-      } else {
-        body.data.forEach(function(data){
-          $("#doctorList").append(`<li>${data.practices[0].name}, ${data.practices[0].visit_address.street}</li>`)
-        });
-      }
+      displayData(response);
     }, function(error) {
       $("#error").text(`${error.message}`)
     });
@@ -35,16 +28,20 @@ $(document).ready(function() {
     reset();
     let promise = doctor.findByName($("#name").val());
     promise.then(function(response) {
-      let body = JSON.parse(response);
-      if(body.data.length < 1) {
-        $("#error").text(`I'm sorry, there are no doctors in your area matching your search query.`)
-      } else {
-        body.data.forEach(function(data){
-          $("#doctorList").append(`<li>${data.practices[0].name}, ${data.practices[0].visit_address.street}</li>`)
-        });
-      }
+      displayData(response);
     }, function(error) {
       $("#error").text(`${error.message}`)
     });
   });
+
+  function displayData(response) {
+    let body = JSON.parse(response);
+    if(body.data.length < 1) {
+      $("#error").text(`I'm sorry, there are no doctors in your area matching your search query.`)
+    } else {
+      body.data.forEach(function(data){
+        $("#doctorList").append(`<li>${data.practices[0].name}, ${data.practices[0].visit_address.street}</li>`)
+      });
+    }
+  }
 });
