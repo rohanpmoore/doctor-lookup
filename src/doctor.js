3c900ada06ea154dffde38cde1a5102102ccdaf3
Rohan Moore <rohanpmoore@gmail.com>
2019-01-18 12:42:17 -0800

    add code to meet first requirement, trying to fix local issue

--- src/doctor.js
+++ src/doctor.js
@@ -1,5 +1,17 @@
 export class Doctor {
-  constructor() {
-
+  findForIssue(issue) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${issue}&location=45.5122%2C%20-122.6587%2C%2030&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
+import { Doctor } from './doctor.js'
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
+  const doctor = new Doctor();
 
+  function reset() {
+    $("#doctorList").html("");
+    $("#error").html("");
+  }
+
+  $("#searchByMalady").submit(function(event) {
+    event.preventDefault();
+    reset();
+    let promise = doctor.findForIssue($("#malady").val());
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      body.data.forEach(function(data){
+        $("#doctorList").append(`<li>${data.practices.name}, ${data.practices.visit_address.street}</li>`)
+      });
+    }, function(error) {
+      $("#error").text(`${error.message}`)
+    });
+  });
 });
